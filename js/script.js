d44e21142c163e8349f8fc8674115f11d845befc
Arnaldo Cisneros <arnaldo.jose.cisneros.zambrano@gmail.com>
2021-06-21 13:41:17 -0500

    Added responsive navigation bar

--- js/script.js
+++ js/script.js
@@ -1,34 +1,62 @@
 let menuDeploy = false;
 let subMenus = []
 
+window.onload = function() {            
+    function checkScreenWidth() {
+        if (window.innerWidth >= 1024) {
+            subMenus= document.getElementsByClassName('menu--item')
+            for (i = 0; i < subMenus.length ;i++) {
+                subMenus[i].style.display='inline-block';
+            }
+        } else {
+            if (!menuDeploy) {
+                subMenus= document.getElementsByClassName('menu--item')
+                for (i = 0; i < subMenus.length ;i++) {
+                    subMenus[i].style.display='none';
+                } 
+            } else {
+                subMenus= document.getElementsByClassName('menu--item')
+                for (i = 0; i < subMenus.length ;i++) {
+                    subMenus[i].style.display='block';
+                } 
+            }
+        }
+    }
+    setInterval(checkScreenWidth, 1);
+}
+
 function clickMenuButton () {
-    if (menuDeploy) {
-        // Code to hide menu
-        subMenus= document.getElementsByClassName('menu--item')
-        for (i = 0; i < subMenus.length ;i++) {
-            subMenus[i].style.display='none';
+    if (document.getElementById("menu-button").style.display != 'none' && window.innerWidth<1024) {
+        if (menuDeploy) {
+            // Code to hide menu
+            subMenus= document.getElementsByClassName('menu--item')
+            for (i = 0; i < subMenus.length ;i++) {
+                subMenus[i].style.display='none';
+            }
+            document.getElementById('menu-arrow').style.transform='rotate(0deg)'
+            menuDeploy=false;
+        } else {
+            // Code to show menu
+            subMenus= document.getElementsByClassName('menu--item')
+            for (i = 0; i < subMenus.length ;i++) {
+                subMenus[i].style.display='block';
+            }
+            document.getElementById('menu-arrow').style.transform='rotate(180deg)'
+            menuDeploy=true;
         }
-        document.getElementById('menu-arrow').style.transform='rotate(0deg)'
-        menuDeploy=false;
-    } else {
-        // Code to show menu
-        subMenus= document.getElementsByClassName('menu--item')
-        for (i = 0; i < subMenus.length ;i++) {
-            subMenus[i].style.display='block';
-        }
-        document.getElementById('menu-arrow').style.transform='rotate(180deg)'
-        menuDeploy=true;
     }
 }
 
 function hideMenu() {
-    if (menuDeploy) {
-        // Code to hide menu
-        subMenus= document.getElementsByClassName('menu--item')
-        for (i = 0; i < subMenus.length ;i++) {
-            subMenus[i].style.display='none';
-        }
-        document.getElementById('menu-arrow').style.transform='rotate(0deg)'
-        menuDeploy=false;
-    } 
+    if (document.getElementById("menu-button").style.display != 'none' && window.innerWidth<1024)  {
+        if (menuDeploy) {
+            // Code to hide menu
+            subMenus= document.getElementsByClassName('menu--item')
+            for (i = 0; i < subMenus.length ;i++) {
+                subMenus[i].style.display='none';
+            }
+            document.getElementById('menu-arrow').style.transform='rotate(0deg)'
+            menuDeploy=false;
+        } 
+    }
 }
