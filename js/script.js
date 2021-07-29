fc9b4b2f95a8a3ab73ae12a0769a7fbacf1a4a3a
Arnaldo Cisneros <arnaldo.jose.cisneros.zambrano@gmail.com>
2021-07-29 18:15:07 -0500

    Slightly modified JS to add some Event Listeners

--- js/script.js
+++ js/script.js
@@ -1,67 +1,63 @@
-let menuDeploy = false;
+const subMenus = document.getElementsByClassName('menu--item')
+const menuArrow = document.getElementById('menu-arrow')
+const menuBtn = document.getElementById("menu-button")
 
+let menuDeploy = false;
 let htmlAndCssDeploy = true;
 let javascriptDeploy = true;
 let pythonDeploy = true;
 let otherDeploy = true;
 
-let subMenus = []
-
-window.onload = function() {            
-    function checkScreenWidth() {
-        if (window.innerWidth >= 1024) {
-            subMenus= document.getElementsByClassName('menu--item')
+const checkScreenWidth = () => {
+    if (innerWidth >= 1024) {
+        for (i = 0; i < subMenus.length ;i++) {
+            subMenus[i].style.display='inline-block';
+        }
+    } else {
+        if (!menuDeploy) {
             for (i = 0; i < subMenus.length ;i++) {
-                subMenus[i].style.display='inline-block';
-            }
+                subMenus[i].style.display='none';
+            } 
         } else {
-            if (!menuDeploy) {
-                subMenus= document.getElementsByClassName('menu--item')
-                for (i = 0; i < subMenus.length ;i++) {
-                    subMenus[i].style.display='none';
-                } 
-            } else {
-                subMenus= document.getElementsByClassName('menu--item')
-                for (i = 0; i < subMenus.length ;i++) {
-                    subMenus[i].style.display='block';
-                } 
-            }
+            for (i = 0; i < subMenus.length ;i++) {
+                subMenus[i].style.display='block';
+            } 
         }
     }
-    setInterval(checkScreenWidth, 1);
 }
 
+addEventListener("load", checkScreenWidth)
+
+addEventListener("resize", checkScreenWidth)
+
 function clickMenuButton () {
-    if (document.getElementById("menu-button").style.display != 'none' && window.innerWidth<1024) {
+    if (menuBtn.style.display != 'none' && innerWidth<1024) {
         if (menuDeploy) {
             // Code to hide menu
-            subMenus= document.getElementsByClassName('menu--item')
             for (i = 0; i < subMenus.length ;i++) {
                 subMenus[i].style.display='none';
             }
-            document.getElementById('menu-arrow').style.transform='rotate(0deg)'
+            menuArrow.style.transform='rotate(0deg)'
             menuDeploy=false;
         } else {
             // Code to show menu
-            subMenus= document.getElementsByClassName('menu--item')
             for (i = 0; i < subMenus.length ;i++) {
                 subMenus[i].style.display='block';
             }
-            document.getElementById('menu-arrow').style.transform='rotate(180deg)'
+            menuArrow.style.transform='rotate(180deg)'
             menuDeploy=true;
         }
     }
 }
 
 function hideMenu() {
-    if (document.getElementById("menu-button").style.display != 'none' && window.innerWidth<1024)  {
+    if (menuBtn.style.display != 'none' && innerWidth<1024)  {
         if (menuDeploy) {
             // Code to hide menu
-            subMenus= document.getElementsByClassName('menu--item')
             for (i = 0; i < subMenus.length ;i++) {
                 subMenus[i].style.display='none';
             }
-            document.getElementById('menu-arrow').style.transform='rotate(0deg)'
+            menuArrow.style.transform='rotate(0deg)'
             menuDeploy=false;
         } 
     }
